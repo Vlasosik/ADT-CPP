13b0ccf22c8f80b656e7a456f3b6af2bb0484908
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-10-16 12:55:07 +0300

    added include file binary_tree/Tree.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <initializer_list>
 #include "forward_list/ForwardList.h"
 #include "linked_list/LinkedList.h"
 #include "queue/Queue.h"
 #include "stack/Stack.h"
+#include "binary_tree/BinaryTree.h"
+
 
 int main() {
-      return 0;
+    return 0;
 }
